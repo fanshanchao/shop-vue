814ce68d54ef204910271d46e51b636bf2871719
fanshanchao <1049709821@qq.com>
2019-11-13 13:50:29 +0800

    hhh

--- src/request/api/base.js
+++ src/request/api/base.js
@@ -1,8 +1,8 @@
 //接口域名的管理
 const base = {
 	//服务端地址
-	lo:'http://120.76.56.254:80/myweb/',
-	//lo:'http://localhost:8081/shop/',
+	//lo:'http://120.76.56.254:8080/myweb/',
+	lo:'http://localhost:8081/shop/',
 	//nginx代理服务器静态资源图片所在的路径
 	image:'http://120.76.56.254:8083/images/',
 	//elasticsearch商品节点服务器所在的地址,
